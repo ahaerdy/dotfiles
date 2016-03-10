29fbeda276e6b1eb804f473f14da80ec47fbb21c
gotbletu <gotbletu@gmail.com>
2016-03-10 12:43:27 -0800

    March update v2

--- cmus/.cmus/playlist.pl
+++
@@ -1,5 +0,0 @@
-/home/heoyea-core/Music/Soundtrack/Fast and Furious 4 (2009)/Tasha - Fast & Furious - 10 - La Isla Bonita.mp3
-/home/heoyea-core/Music/Soundtrack/Romance of The Condor Heroes 2014 OST/Romance of the Condor Heroes 2014 - Zhang Jie 張杰 - 02 - Vastness 浩瀚(電視劇《神雕俠侶》片頭主題曲).mp3
-/home/heoyea-core/Music/Soundtrack/Romance of The Condor Heroes 2014 OST/Romance of the Condor Heroes 2014 - 張杰 - 05 - Vastness (Instrumental) 浩瀚.mp3
-/home/heoyea-core/Music/GreatestHits/Technotronic - Pump Up the Jam.mp3
-/home/heoyea-core/Music/Soundtrack/Fast and Furious 5 (2011)/Ludacris Featuring Slaughterhouse & Claret Jai - Fast Five - 13 - Furiously Dangerous.mp3
